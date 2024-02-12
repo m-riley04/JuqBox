170a3e4e09ff5b5e29c0fc7116f506766782c052
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-12 12:56:09 -0600

    Add basic layout of joining a room

--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -1,6 +1,33 @@
+import { Button, Form } from "react-bootstrap";
+
 function JoinRoom() {
+    function handleChangeRoomCode() {
+
+    }
+
+    function handleClickedCamera() {
+        console.log("Opening camera for QR code...")
+    }
+
+    function handleJoinRoom(event?) {
+        // Stop page refreshing
+        event?.preventDefault();
+
+        
+        console.log("Attempting to join room...")
+    }
+
     return (
         <>
+            <Form onSubmit={handleJoinRoom}>
+                <Form.Group controlId="roomCode">
+                    <Form.Label>Room Code</Form.Label>  
+                    <Form.Control type="name" placeholder="Enter the room code here..." onChange={handleChangeRoomCode}></Form.Control>
+                    <Button onClick={handleClickedCamera}>QR</Button>
+                </Form.Group>
+            </Form>
+
+            <Button onClick={handleJoinRoom}>Join Room</Button>
         </>
     );
 }
