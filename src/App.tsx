52fc5381c34f1e76f8870392be22dd19b98096a2
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-12 12:55:33 -0600

    add paths for login, account, and create-account

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HostRoom from "./pages/HostRoom";
 import JoinRoom from "./pages/JoinRoom";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
 import NavigationBar from "./components/NavigationBar";
 
 
@@ -15,6 +18,9 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path="host" element={<HostRoom/>}/>
           <Route path="join" element={<JoinRoom/>}/>
+          <Route path="account" element={<Account/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="create-account" element={<CreateAccount/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
