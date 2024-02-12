41fccb3a3f9f7045f08f5e9ff08948f77e67f1d4
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-12 12:56:16 -0600

    add basic layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,21 @@
+import { Button, Form } from "react-bootstrap";
+
 function Login() {
     return (
         <>
+            <Form>
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username"></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password"></Form.Control>
+                </Form.Group>
+
+                <Button>Login</Button>
+            </Form>
         </>
     );
 }
