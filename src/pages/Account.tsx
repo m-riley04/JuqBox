445c0a1753706c629e2c8e50fc254714ac604adb
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-12 12:55:41 -0600

    add basic layout for account

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,6 +1,21 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
+
 function Account() {
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        navigate("../login");
+    }
+
+    function handleCreateAccount() {
+        navigate("../create-account");
+    }
+
     return (
         <>
+            <Button onClick={handleLogin}>Login</Button>
+            <Button onClick={handleCreateAccount}>Create Account</Button>
         </>
     );
 }
