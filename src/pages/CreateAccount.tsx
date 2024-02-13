580d37f59c1876055890028267b20d641d556ef3
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-12 22:01:48 -0600

    add handlers and hooks

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,36 +1,78 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 function CreateAccount() {
-    function handleCreateAccount() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [confirmedEmail, setConfirmedEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmedPassword, setConfirmedPassword] = useState("");
+    const [emailsMatch, setEmailsMatch] = useState(false);
+    const [passwordsMatch, setPasswordsMatch] = useState(false);
+
+    function handleCreateAccount(event) {
 
     }
-    
+
+    function handleChangedUsername(event) {
+        setUsername(event.target.value);
+        console.log(username)
+    }
+
+    function handleChangedEmail(event) {
+        setEmail(event.target.value);
+        setEmailsMatch(email == confirmedEmail);
+        if (email == confirmedEmail) {
+            console.log("Match")
+            event.target.style = { backgroundColor: "red"}
+        }
+        console.log(email)
+    }
+
+    function handleChangedConfirmedEmail(event) {
+        setConfirmedEmail(event.target.value);
+        setEmailsMatch(email === confirmedEmail);
+        console.log(confirmedEmail);
+    }
+
+    function handleChangedPassword(event) {
+        setPassword(event.target.value);
+        setPasswordsMatch(password === confirmedPassword);
+        console.log(password)
+    }
+
+    function handleChangedConfirmedPassword(event) {
+        setConfirmedPassword(event.target.value);
+        setPasswordsMatch(password === confirmedPassword);
+        console.log(confirmedPassword)
+    }
+
     return (
         <>
             <Form>
                 <Form.Group controlId="username">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="username"></Form.Control>
+                    <Form.Control type="username" onChange={handleChangedUsername}></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email"></Form.Control>
+                    <Form.Control type="email" onChange={handleChangedEmail}></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId="confirmEmail">
                     <Form.Label>Confirm Email</Form.Label>
-                    <Form.Control type="email"></Form.Control>
+                    <Form.Control type="email" onChange={handleChangedConfirmedEmail}></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password"></Form.Control>
+                    <Form.Control type="password" onChange={handleChangedPassword}></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId="confirmPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password"></Form.Control>
+                    <Form.Control type="password" onChange={handleChangedConfirmedPassword}></Form.Control>
                 </Form.Group>
             </Form>
             <Button onClick={handleCreateAccount}>Create Account</Button>
