a74d9dbcc8a1e7aadc5df9b837a0996b8d8a4266
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-14 00:30:06 -0600

    add env variables docstrings

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_DATABASE_URL: string,
+    readonly VITE_ADMIN_PASSWORD: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
