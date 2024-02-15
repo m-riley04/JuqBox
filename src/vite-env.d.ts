659725ecaac09559e33e67f3a19d6016cbf0bf2d
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-14 18:20:44 -0600

    add docstrings for environmental variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_DATABASE_URL: string,
-    readonly VITE_ADMIN_PASSWORD: string
+    readonly VITE_ADMIN_PASSWORD: string,
+    readonly VITE_SPOTIFY_CLIENT_ID: string,
+    readonly VITE_SPOTIFY_SECRET: string
 }
 
 interface ImportMeta {
