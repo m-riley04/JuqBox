a6a8000b6fb9b21a079bbb1053f21fbea895a0ad
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-15 15:11:44 -0600

    add more environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,13 @@ interface ImportMetaEnv {
     readonly VITE_ADMIN_PASSWORD: string,
     readonly VITE_SPOTIFY_CLIENT_ID: string,
     readonly VITE_SPOTIFY_SECRET: string
+    readonly VITE_DATABASE_HOST: string,
+    readonly VITE_DATABASE_USERNAME: string,
+    readonly VITE_DATABASE_PASSWORD: string,
+    readonly VITE_AUTH0_DOMAIN: string,
+    readonly VITE_AUTH0_CLIENT_ID: string,
+    readonly VITE_AUTH0_SECRET: string,
+    readonly VITE_AUTH0_AUDIENCE: string,
 }
 
 interface ImportMeta {
