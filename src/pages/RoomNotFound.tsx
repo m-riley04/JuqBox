3a05fe3d4e9b2dfb13ffaf29721f25b81a04a3d3
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-15 12:08:06 -0600

    Create RoomNotFound.tsx

---
+++ src/pages/RoomNotFound.tsx
@@ -0,0 +1,9 @@
+function RoomNotFound() {
+    return (
+        <>
+            <p>The room you are trying to connect to doesn't exist. Please try again.</p>
+        </>
+    );
+}
+
+export default RoomNotFound;
