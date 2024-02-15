6d85aee36d7f2dce0428dbd5f9a95e341b4b0e1a
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-15 00:52:13 -0600

    implement basic back-end integration

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,44 +1,141 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { User } from "../../netlify/functions/database";
+import { useNavigate } from "react-router-dom";
 
 function CreateAccount() {
+    const navigate = useNavigate();
+
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [confirmedEmail, setConfirmedEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
-
-    function handleCreateAccount() {
-
-    }
 
     function handleChangedUsername(event) {
         setUsername(event.target.value);
-        console.log(username)
     }
 
     function handleChangedEmail(event) {
         setEmail(event.target.value);
-        if (email == confirmedEmail) {
-            console.log("Match")
-            event.target.style = { backgroundColor: "red"}
+        if (event.target.value == confirmedEmail) {
+            console.log("Emails now match")
         }
-        console.log(email)
     }
 
     function handleChangedConfirmedEmail(event) {
         setConfirmedEmail(event.target.value);
-        console.log(confirmedEmail);
+        if (event.target.value == email) {
+            console.log("Emails now match")
+        }
     }
 
     function handleChangedPassword(event) {
         setPassword(event.target.value);
-        console.log(password)
     }
 
-    function handleChangedConfirmedPassword(event) {
-        setConfirmedPassword(event.target.value);
-        console.log(confirmedPassword)
+    function addUser(user: User) {
+        const body = JSON.stringify({
+            tableName: "users",
+            values: user
+        })
+        fetch('/.netlify/functions/database/addRow', {
+            method: 'POST',
+            body
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not okay");
+            }
+            console.log("User added successfully!");
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
+
+    async function isUsernameTaken(username: string) {
+        console.log("Checking username...");
+
+        try {
+            const response = await fetch(`/.netlify/functions/database/checkUserUsername/${username}`, { method: 'GET' })
+            const result = await response.json();
+
+            if (result.rows.length > 0) {
+                return true;
+            }
+            return false;
+
+        } catch (error) {
+            console.error(`Unable to check username: ${error}`);
+            return true;
+        }
+    }
+
+    async function isEmailTaken(email: string) {
+        console.log("Checking email...");
+
+        try {
+            const response = await fetch(`/.netlify/functions/database/checkUserEmail/${email}`, { method: 'GET' })
+            const result = await response.json();
+
+            if (result.rows.length > 0) {
+                return true;
+            }
+            return false;
+
+        } catch (error) {
+            console.error(`Unable to check email: ${error}`);
+            return true;
+        }
+    }
+
+    async function getNewUserId() {
+        console.log("Getting new user ID...");
+        
+        try {
+            const response = (await fetch(`/.netlify/functions/database/getUserCount`, { method: "GET" } ));
+            const result = await response.json();
+
+            return (Number(result.rows[0]["count(*)"]) + 1);
+        } catch (error) {
+            console.error(`Unable to get new user id: ${error}`);
+            return 0;
+        }
+    }
+
+    async function handleAddUser() {
+        const usernameTaken = await isUsernameTaken(username);
+        const emailTaken = await isEmailTaken(email);
+        const id = await getNewUserId();
+
+        // Check if both are taken
+        if (usernameTaken && emailTaken) {
+            console.warn("Username and email are already in use.");
+            return;
+        }
+
+        // Check if the username is taken
+        if (usernameTaken) {
+            console.warn("Username has already been taken.")
+            return;
+        }
+
+        // Check if the email is taken
+        if (emailTaken) {
+            console.warn("Email is already being used on another account.")
+            return;
+        }
+
+        // If the checks pass, add the user to the database
+        addUser({
+            id: id,
+            username: username,
+            email: email,
+            password_hash: password
+        });
+
+        // Redirect to
+        navigate()
     }
 
     return (
@@ -64,12 +161,8 @@ function CreateAccount() {
                     <Form.Control type="password" onChange={handleChangedPassword}></Form.Control>
                 </Form.Group>
 
-                <Form.Group controlId="confirmPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" onChange={handleChangedConfirmedPassword}></Form.Control>
-                </Form.Group>
             </Form>
-            <Button onClick={handleCreateAccount}>Create Account</Button>
+            <Button onClick={handleAddUser}>Create Account</Button>
         </>
     );
 }
