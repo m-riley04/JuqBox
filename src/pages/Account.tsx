6e3590ca9453043cc1213bcd866604c6535919a2
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-16 13:15:38 -0600

    add popup login and authentication

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,10 +2,34 @@ import LogoutButton from "../components/Auth0/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "../components/Auth0/Profile";
 
-
 function Account() {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, error, isLoading } = useAuth0();
+    const { loginWithPopup } = useAuth0();
 
+    if (isLoading) return (
+        <>
+            <p>Loading...</p>
+        </>
+    );
+
+    if (error) return (
+        <>
+            <h1>Error</h1>
+            <p>There was an error logging you in: {error.message}</p>
+        </>
+    );
+
+    if (!isAuthenticated) {
+        loginWithPopup(); // Log in with a popup window (might be less accessible)
+        //loginWithRedirect(); // Log in with a redirect (lose the app's states)
+
+        return (
+            <>
+                <p>Waiting for login...</p>
+            </>
+        );
+    }
+    
     if (isAuthenticated) return (
         <>
             <Profile></Profile>
