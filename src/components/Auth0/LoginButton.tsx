a6d8429fd30b7d48040807a0f58b69e9a957d50d
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-16 17:23:36 -0600

    Update LoginButton.tsx

--- src/components/Auth0/LoginButton.tsx
+++ src/components/Auth0/LoginButton.tsx
@@ -1,10 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
 
   return (
-    <button onClick={() => loginWithRedirect()}>Log In</button>
+    <button onClick={() => loginWithPopup()}>Log In</button>
   );
 };
 
