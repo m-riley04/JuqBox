1a3be75afe6fe197c6405c579b4c99013a0b551b
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-19 01:16:47 -0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import record from "../assets/juqbox_record.svg";
+import record from "../../assets/juqbox_record.svg";
 
 function Home() {
     const navigate = useNavigate();
