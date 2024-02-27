b641f3c24fc042ef9205e76bc8c02b77ec186e7c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-27 13:47:18 -0600

    remove Admin page from user's access

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -12,7 +12,6 @@ function NavigationBar() {
                         <Nav.Link as={Link} to="host">Host</Nav.Link>
                         <Nav.Link as={Link} to="join">Join</Nav.Link>
                         <Nav.Link as={Link} to="account">Account</Nav.Link>
-                        <Nav.Link as={Link} to="admin">Admin</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
