75a125fbedbd47fb1dd383ec98fe21bb5f50466c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-29 12:55:52 -0600

    add qr code reader modal

--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -1,19 +1,27 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { doesRoomExist } from "../server/roomRequests";
+import { QrReader } from "react-qr-reader";
 
 function JoinRoom() {
     const navigate = useNavigate();
     
     const [code, setCode] = useState(100000000);
+    const [showScanner, setShowScanner] = useState(false);
     
     function handleChangeRoomCode(event: ChangeEvent<HTMLInputElement>){
         setCode(Number(event.target.value));
     }
 
-    function handleClickedCamera() {
+    function handleOpenCamera() {
         console.log("Opening camera for QR code...")
+        setShowScanner(true)
+    }
+    
+    function handleCloseCamera() {
+        setShowScanner(false)
+        window.location.reload();
     }
 
     async function handleJoinRoom(event: FormEvent) {
@@ -32,12 +40,36 @@ function JoinRoom() {
 
     return (
         <>
+            <Modal show={showScanner}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Scan Code</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <QrReader
+                        onResult={(result, error) => {
+                            if (result) {
+                                //
+                            }
+
+                            if (error) {
+                                console.info(error);
+                            }
+                        }}
+                    />
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseCamera}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+
             <Form onSubmit={handleJoinRoom} id="form-join">
                 <Form.Group controlId="roomCode">
                     <Form.Label style={{display: "block"}}>Room Code</Form.Label>
                     <div style={{display: "inline-flex", boxSizing: "border-box", width: "100%"}}>
                         <Form.Control type="name" placeholder="Enter the room code here..." onChange={handleChangeRoomCode}></Form.Control>
-                        <Button onClick={handleClickedCamera}>QR</Button>
+                        <Button onClick={handleOpenCamera} >QR</Button>
                     </div>
                 </Form.Group>
                 <Button onClick={handleJoinRoom} style={{boxSizing: "border-box", width: "100%"}}>Join Room</Button>
