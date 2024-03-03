cf06a4c38d565424b5e1ff6019b359b02eb38fc1
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-02 18:40:27 -0600

    move qr code up

--- src/pages/RoomHost.tsx
+++ src/pages/RoomHost.tsx
@@ -98,12 +98,12 @@ function RoomHost() {
     if (roomExists && userOwnsRoom) return (
         <>
             <h1>Code: {params.code}</h1>
-            <p>Join the queue now at <a href="www.juqbox.space/join" target="_blank" rel="noreferrer">juqbox.space/join</a></p>
-            <p>Current Guests:</p>
-            {guests.map((guest, index) => <p key={index}>{index+1}. {guest.name}</p>)}
             <QRCode
                 value={`${window.location.origin}/join/${params.code}`}
             ></QRCode>
+            <p>Join the queue now at <a href="www.juqbox.space/join" target="_blank" rel="noreferrer">juqbox.space/join</a></p>
+            <p>Current Guests:</p>
+            {guests.map((guest, index) => <p key={index}>{index+1}. {guest.name}</p>)}
             <button onClick={handleClickCloseRoom}>Close Room</button>
             <button onClick={handleClickSettings}>Settings</button>
         </>
