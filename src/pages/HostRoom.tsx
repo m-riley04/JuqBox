b20120c9555aba6282902865270dbc362a19cd6b
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-02 22:16:14 -0600

    change back to values

--- src/pages/HostRoom.tsx
+++ src/pages/HostRoom.tsx
@@ -53,10 +53,10 @@ function HostRoom() {
             name: roomName || `${user?.nickname}'s Room`,
             owner: user?.sub || "",
             max_guests: roomMaxGuests,
-            guests: { guests: [] },
+            guests: JSON.stringify({ guests: [] }),
             max_queues_per_guest: roomMaxCoexistentQueues,
             queue_cost: roomQueueCost,
-            creation_date: new Date().toISOString()
+            creation_date: undefined
         })
 
         // Redirect to another page
