2d0e172eccf3905cf0c998b12b55351fc3a9856c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-09 13:25:25 -0600

    add track interface

--- src/server/spotifyRequests.ts
+++ src/server/spotifyRequests.ts
@@ -8,6 +8,27 @@ export interface Artist {
     type: string;
     uri: string;
 }
+
+export interface Track {
+    album: object;
+    artists: Array<Artist>;
+    available_markets: Array<string>;
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: object;
+    external_urls: object;
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string | undefined;
+    track_number: number;
+    type: string;
+    uri: string;
+}
+
         .then(response => {
             if (!response.ok) {
                 throw new Error("Track query response was not okay")
