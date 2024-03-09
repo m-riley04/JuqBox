12df80bfba54f070174f002289de59805db92f20
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-09 13:25:16 -0600

    add artist interface

--- src/server/spotifyRequests.ts
+++ src/server/spotifyRequests.ts
@@ -1,5 +1,13 @@
 export function getTracks(name:string, token:string) {
     return fetch(`https://api.spotify.com/v1/search?q=remaster%2520track%3A${name}&type=track`, { headers: {Authorization: `Bearer ${token}`}})
+export interface Artist {
+    external_urls: object;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+}
         .then(response => {
             if (!response.ok) {
                 throw new Error("Track query response was not okay")
